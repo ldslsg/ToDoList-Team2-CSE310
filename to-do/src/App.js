34bbb07b71c627fe18e8b2f173e4ff697c7963fd
Ruth <144852850+Ruth-Jensen@users.noreply.github.com>
2024-05-20 17:53:37 -0600

    fixed side bar and some design

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,44 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+// images
+import logo from './images/checkmate-dark-02.png';
+import name from './images/checkmate-name-dark.png';
+// icons
 import { IoSearchCircle } from "react-icons/io5";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { FaAnglesLeft } from "react-icons/fa6";
-import React from 'react';
+import { FaAnglesRight } from "react-icons/fa6";
+
+import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <div className="App">
+
       <header className="App-header">
 
+        {/* <div className='sidebar-header'>
+          <img src={logo} className="app-logo" alt="logo" />
+          <RxHamburgerMenu />
+          <button onClick={toggleSidebar} className="toggle-button">
+            {isSidebarOpen ? <FaAnglesLeft /> : <FaAnglesRight />}
+          </button>
+        </div> */}
 
-        <img src="https://fakeimg.pl/50x50" className="app-logo" alt="logo" />
-        <h3> <RxHamburgerMenu /> </h3>
-        <h3> <FaAnglesLeft /> </h3>
-        <h1>Check Mate</h1>
-        {/* text box */}
-        <h3> <IoSearchCircle /> </h3>
-        <i class="fa fa-check"></i>
-        <i class="fa fa-check"></i>
-
-        {/* <text>search bar<i class="fa fa-check"></i></text> */}
-
-        <h2>username</h2>
-        <img src="https://fakeimg.pl/50x50" className="user-img" alt="user img" />
-
-
-
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        <img src={name} className="app-name" alt="Check Mate" />
+        <IoSearchCircle />
+        <i className="fa fa-check"></i>
+        <i className="fa fa-check"></i>
+        <h5>username</h5>
+        <img src="https://fakeimg.pl/70x70" className="user-img" alt="user img" />
       </header>
-      <main>
 
+      <div className={`panel ${isSidebarOpen ? '' : 'collapsed'}`}>
+
+        <div className='sidebar-header'>
+          <img src={logo} className="app-logo" alt="logo" />
+          <RxHamburgerMenu />
+          <button onClick={toggleSidebar} className="toggle-button">
+            {isSidebarOpen ? <FaAnglesLeft /> : <FaAnglesRight />}
+          </button>
+        </div>
+        
+        <h2>Check Lists</h2>
+        <a href="#">List 1</a>
+        <a href="#">List 2</a>
+        <a href="#">List 3</a>
+        <a href="#">List 4</a>
+        <a href="#">List 5</a>
+        <a href="#">List 6</a>
+      </div>
+      
+      <main className="content">
+        <img src="https://fakeimg.pl/70x70" className="user-img" alt="user img" />
       </main>
       <footer>
 
