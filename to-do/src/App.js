c68870a951f3f9259135e766f49014517fcdf2ca
Ruth <144852850+Ruth-Jensen@users.noreply.github.com>
2024-05-20 11:16:01 -0600

    grid

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,28 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import { IoSearchCircle } from "react-icons/io5";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { FaAnglesLeft } from "react-icons/fa6";
 import React from 'react';
-
-class Question extends React.Component {
-  render() {
-    return <h3> <IoSearchCircle /> </h3>
-  }
-}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+
         <img src="https://fakeimg.pl/50x50" className="app-logo" alt="logo" />
-        <Question>hello</Question>
+        <h3> <RxHamburgerMenu /> </h3>
+        <h3> <FaAnglesLeft /> </h3>
+        <h1>Check Mate</h1>
+        {/* text box */}
+        <h3> <IoSearchCircle /> </h3>
         <i class="fa fa-check"></i>
         <i class="fa fa-check"></i>
 
-        <h1>Check Mate</h1>
-        <text>search bar<i class="fa fa-check"></i></text>
+        {/* <text>search bar<i class="fa fa-check"></i></text> */}
 
         <h2>username</h2>
         <img src="https://fakeimg.pl/50x50" className="user-img" alt="user img" />
+
+
+
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         {/* <a
           className="App-link"
