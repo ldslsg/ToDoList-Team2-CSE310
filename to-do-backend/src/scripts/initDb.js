49c398ac99d597694e99746150df98f084a1a54b
student-i <ladyannamarie@gmail.com>
2024-06-04 23:24:45 -0600

    database structure implimented

--- to-do-backend/src/scripts/initDb.js
+++ to-do-backend/src/scripts/initDb.js
@@ -3,15 +3,32 @@ const { openDb } = require('../db');
 const initializeDatabase = async () => {
   const db = await openDb();
   try {
-    await db.exec(`
-      CREATE TABLE IF NOT EXISTS todos (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        description TEXT NOT NULL,
-        status TEXT NOT NULL CHECK (status IN ('pending', 'completed')),
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-      )
-    `);
-    console.log('Database initialized for TO-DO list app');
+     await db.exec(`
+      CREATE TABLE IF NOT EXISTS user (
+      user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+      email TEXT NOT NULL,
+      password TEXT NOT NULL)
+     `);
+     await db.exec(`
+      CREATE TABLE IF NOT EXISTS list (
+        list_id INTEGER PRIMARY KEY AUTOINCREMENT,
+         list_title TEXT NOT NULL,
+        list_creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        user_id INTEGER NOT NULL,
+         FOREIGN KEY(user_id) REFERENCES user(user_id))
+     `);
+     await db.exec(`
+      CREATE TABLE IF NOT EXISTS to_dos (
+      to_dos_id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      description TEXT NOT NULL,
+      deadline_date TIMESTAMP NOT NULL,
+      status INTEGER NOT NULL,
+      priority INTEGER NOT NULL,
+      list_id INTEGER NOT NULL,
+      FOREIGN KEY(list_id) REFERENCES list(list_id))
+     `);
+    console.log('Database initialized');
   } catch (err) {
     console.error('Error initializing database:', err);
   } finally {
@@ -22,3 +39,4 @@ const initializeDatabase = async () => {
 initializeDatabase().catch((err) => {
   console.error('Error initializing database:', err);
 });
+
