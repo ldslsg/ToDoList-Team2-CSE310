0db9c15a595a31c408dcfdd6af2fc2804040d3da
EmeraldE11 <77083262+EmeraldE11@users.noreply.github.com>
2024-07-12 21:50:19 -0600

    created and implemented light and dark modes, and a button to use switch between them.

---
+++ to-do/src/ThemeContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useState, useContext, useEffect } from "react";
+
+const ThemeContext = createContext();
+
+export const useTheme = () => useContext(ThemeContext);
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('dark'); // Set the default theme to 'dark'
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
